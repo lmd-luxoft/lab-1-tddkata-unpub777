90f40c2292a1f8d99c768321a5c3cc882dc3472d
stseplukhin <sergey.tseplukhin@excelian.com>
2020-12-07 16:10:31 +0300

    implementing

--- TDDKata/StringCalc.cs
+++ TDDKata/StringCalc.cs
@@ -1,13 +1,15 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
 using System;
+using System.Text.RegularExpressions;
 
 namespace TDDKata
 {
     internal class StringCalc
     {
-        private const char SEPARATOR1 = ',';
-        private const char SEPARATOR2 = '\n';
+        private const string DECLARE_CUSTOM_SEPARATOR_PATTERN = "(?<=//).+(?=\\n)";
+        private string SEPARATOR1 = ",";
+        private string SEPARATOR2 = "\n";
         private const int ERROR = -1;
         private const int EMPTY = 0;
 
@@ -20,7 +22,9 @@ namespace TDDKata
 
             var result = EMPTY;
 
-            foreach(var item in v.Split(SEPARATOR1, SEPARATOR2))
+            var separators = GetSeparators(v);
+
+            foreach (var item in GetString(v).Split(separators, StringSplitOptions.RemoveEmptyEntries))
             {
                 if (!int.TryParse(item.Trim(), out var number) || number < 0)
                 {
@@ -31,6 +35,22 @@ namespace TDDKata
             }
 
             return result;
+        }
+
+        private string GetString(string input)
+        {
+            return Regex.Replace(input, @"//.+\n", string.Empty);
+        }
+
+        private string[] GetSeparators(string input)
+        {
+            var match = Regex.Match(input, DECLARE_CUSTOM_SEPARATOR_PATTERN);
+            if (match.Success)
+            {
+                return new[] { match.Value };
+            }
+
+            return new[] { SEPARATOR1, SEPARATOR2 };
         }
     }
 }

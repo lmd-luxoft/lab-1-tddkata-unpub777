6e6aabee94ed0185d6bbfabdb725b53165b102b1
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-12-07 13:57:49 +0300

    Initial commit

---
+++ TDDKata/StringCalc.cs
@@ -0,0 +1,14 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System;
+
+namespace TDDKata
+{
+    internal class StringCalc
+    {
+        internal int Sum(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDDKata/TestClass.cs
@@ -0,0 +1,20 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TDDKata
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("2,2");
+            Assert.That(value, Is.EqualTo(4), "Wrong actual value");
+        }
+    }
+}
